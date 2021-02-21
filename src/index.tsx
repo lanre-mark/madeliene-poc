715f9d05d02f9eeea6415722d12f20753ea090cc
olanrewaju Makinde <lanre.developer@gmail.com>
2021-02-21 14:05:30 +0100

    setting initial project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*
+  tighten the root element in the index.html
+  index.html must have been loaded before App mounts
+*/
+function initializeInventoryApp() {
+  const appRoot = document.getElementById('root');
+  if (!appRoot) throw new Error('No #root found in DOM');
+  render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    appRoot
+  );
+}
+
+initializeInventoryApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
