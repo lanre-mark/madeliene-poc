4e393fb4cd85173dcb1328a692ae37b6c1b76ff7
olanrewaju Makinde <lanre.developer@gmail.com>
2021-02-21 20:36:19 +0100

    toast-notification: added shortid package and svg fxnal icons

---
+++ src/components/toast/icons/index.tsx
@@ -0,0 +1,4 @@
+import OutofstockIcon from './outofstock';
+import RestockInfo from './restock';
+
+export {OutofstockIcon, RestockInfo}
---
+++ src/components/toast/icons/outofstock.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import shortid from 'shortid';
+
+const id = `outofstock_${shortid.generate()}`;
+const Icon: React.FunctionComponent<any> = ({ title, desc, fillColor1, fillColor2, ...props }: {title: string, desc: string, fillColor1: string, fillColor2: string}) => {
+  return (
+    <svg
+      id={id}
+      {...props}
+      width="32" 
+      height="32" 
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <title id={`${id}_Title`}>{title}</title>
+        <desc id={`${id}_Desc`}>{desc}</desc>
+        <rect fill="none" id={`${id}_canvas_background`} height="402" width="582" y="-1" x="-1"/>
+        </g>
+        <g>
+        <title>{`${id}_Layer_1`}</title>
+        <circle fill={`${fillColor1 || '#ffffff'}`} r="16" id="BG" cy="16" cx="16"/>
+        <path fill={`${fillColor2 || '#d72828'}`} id={`${id}_Exclamatory_x5F_Sign`} d="m14.5,25l3,0l0,-3l-3,0l0,3zm0,-19l0,13l3,0l0,-13l-3,0z"/>
+        </g>
+    </svg>
+
+
+  );
+};
+
+export default Icon;
